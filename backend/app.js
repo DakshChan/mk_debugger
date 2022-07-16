5c17648ccbff5d1f59d3514714ce25c87ba68de5
Daksh <daksh3451@gmail.com>
2022-07-15 21:35:38 -0400

    clean up app.js

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,8 @@ const cors = require('cors');
 const tmp = require('tmp');
 const formData = require('express-form-data');
 
-const { spawn, spawnSync} = require('child_process');
+const { spawn } = require('child_process');
 const fs = require('fs');
-const {parser: jsonlParser} = require('stream-json/jsonl/Parser');
-const testdata = JSON.parse(fs.readFileSync("./sample_response.json"));
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,14 +20,9 @@ app.use(cors({
   origin: '*'
 }));
 
-app.get('/code', (req, res) => {
-  res.send(testdata);
-})
-
 let userCode = null;
 let racket = null;
-let racketbuf = null;
-let racket_stdout_num = 0;
+let racketBuf = null;
 
 const debugger_path = "C:\\Users\\daksh\\WebstormProjects\\mk_debugger\\debugger\\mk-fo.rkt";
 
@@ -38,30 +31,26 @@ app.get('/die', (req, res) => {
     racket.kill();
   }
   racket = null;
-  racketbuf = null;
-  racket_stdout_num = 0;
+  racketBuf = null;
   res.send("killed");
 });
 
 app.post('/code', (req, res) => {
-  //TODO: reformat before sending back to client
+  //TODO: potentially reformat before sending back to client
   userCode = req.files.file.path;
   if (racket !== null) {
     racket.kill();
-    racketbuf = null;
+    racketBuf = null;
   }
-  // let tmpRunnerFile = tmp.fileSync({postfix: '.rkt'});
-  // console.log(tmpRunnerFile.name);
+
+  // let tmpRepl = tmp.fileSync({postfix: '.rkt'});
+  // console.log(tmpRepl.name);
   // const header = `#lang racket\n\n(provide (all-defined-out))\n(require (file "${debugger_path}"))\n\n`.replaceAll("\\", "\\\\");
-  // fs.writeFileSync(tmpRunnerFile.name, header);
-  // fs.appendFileSync(tmpRunnerFile.name, fs.readFileSync(userCode).toString());
-  //
-  // userCode = tmpRunnerFile.name;
+  // fs.writeFileSync(tmpRepl.name, header);
+  // fs.appendFileSync(tmpRepl.name, fs.readFileSync(userCode).toString());
 
   res.sendFile(userCode);
 });
-
-//TODO: use sockets so computation can be interrupted
 
 app.post('/debug', (req, res) => {
   console.log(JSON.stringify(req.body));
@@ -71,126 +60,54 @@ app.post('/debug', (req, res) => {
     return;
   }
 
-
-
-  //TODO: this should be on the client side so that user can see full query being run
-
-  // let solutions = parseInt(req.body.solutions);
-  // solutions = solutions > -1 ? solutions : "#f";
-  // let samples = parseInt(req.body.samples);
-  // samples = samples > -1 ? samples : "#f";
-  // let steps = parseInt(req.body.steps);
-  // steps = steps > -1 ? steps : "#f";
-  // let query = req.body.query;
-
-  // let runStatement = `(run ${solutions} ${samples} ${steps} #t ${query})`;
-  // let resumeStatement = `(resume ${solutions} ${samples} ${steps} #t)`;
-  // console.log(runStatement);
-  // console.log(resumeStatement);
+  racketBuf = "";
+  if (racket !== null) {
+    racket.stdout.removeAllListeners();
+    racket.stderr.removeAllListeners();
+  }
 
   if (req.body.command === "run") { // run
     if (racket !== null) {
       racket.kill();
     }
     racket = spawn("racket.exe", ["repl.rkt"]);
-    racket_stdout_num = 0;
-    // racket.stdin.write(`(require (file "${debugger_path}"))\n`.replaceAll("\\", "\\\\"));
-    // console.log(`(require (file "${debugger_path}"))`.replaceAll("\\", "\\\\"));
-    // racket.stdin.write(`(require (file "${userCode}"))\n`.replaceAll("\\", "\\\\"));
-    // console.log(`(require (file "${userCode}"))`.replaceAll("\\", "\\\\"));
-    // racket.stdin.write(`${runStatement}\n`);
 
   } else { // resume
     if (racket === null) {
       res.status(400).send("No racket process running");
       return;
     }
-    // racket.stdin.write(`${resumeStatement}\n`);
   }
-
-  racketbuf = "";
-  racket.stdout.removeAllListeners();
-  racket.stderr.removeAllListeners();
 
   racket.stdin.write(JSON.stringify(req.body) + "\n");
 
-  // const debugger_path = "C:\\Users\\daksh\\WebstormProjects\\mk_debugger\\debugger\\mk-fo.rkt";
-  //
-  // const code_statement = `#lang racket\n\n(require (file "${debugger_path}"))\n(include (file "${userCode}"))\n${runStatement}`.replaceAll("\\", "\\\\");
-  // console.log(code_statement);
-
-  // const racket = spawnSync('racket.exe', [tmpRunnerFile.name], {maxBuffer: Number.MAX_SAFE_INTEGER});
-  //
-  // if (racket.stderr.toString() !== "") {
-  //   res.status(400).send(racket.stderr.toString());
-  // } else {
-  //   console.log(racket.stdout.toString().length);
-  //   let resultJSON = JSON.parse(racket.stdout.toString());
-  //   // console.log(racket.stdout.toString());
-  //   //filter out initial debugger query
-  //
-  //
-  //   // console.log(JSON.stringify(resultJSON));
-  //   res.send(JSON.stringify(resultJSON));
-  // }
-
-  // racket.stdout.on('data', (data) => {
-  //   console.log(`$stdout: ${data}`);
-  //   racketbuf += data;
-  //   let a;
-  //   while ((a = racketbuf.split("> ")).length > 1) {
-  //     const [f, ...rest] = a;
-  //     console.log(`${racket_stdout_num} stdout: ${f} rest: ${rest.toString()}`);
-  //     console.log(rest);
-  //     if (racket_stdout_num > 2){
-  //       let resultJSON = JSON.parse(f);
-  //       resultJSON["program-points"] = resultJSON["program-points"].filter(x => x.syntax.source !== "stdin")
-  //       resultJSON["rejected-states"] = resultJSON["rejected-states"].map(x => {x.path = x.path.filter(y => y.source !== "stdin"); return x;});
-  //       resultJSON["rejected-states"] = resultJSON["rejected-states"].map(x => {x.stack = x.stack.filter(y => y.source !== "stdin"); return x;});
-  //       resultJSON["solutions"] = resultJSON["solutions"].map(x => {x.path = x.path.filter(y => y.source !== "stdin"); return x;});
-  //       resultJSON["solutions"] = resultJSON["solutions"].map(x => {x.stack = x.stack.filter(y => y.source !== "stdin"); return x;});
-  //       res.json(resultJSON);
-  //       res.end();
-  //     }
-  //     racketbuf = rest.join("> ")
-  //     console.log(racketbuf);
-  //     console.log(`${racket_stdout_num} racketbuf: ${racketbuf}`);
-  //     racket_stdout_num++;
-  //   }
-  // });
-  //
-  // racket.stdout
-  //   .pipe(split(JSON.parse))
-  //   .on('data', (data) => {
-  //     console.log(`$stdout: ${data}`);
-  //   })
-  //   .on('error', (err) => {
-  //     console.log(`$stdout: ${err}`);
-  //   });
-
-  racket.stdout
-    .on('data', (data) => {
-      console.log(`$stdout: ${data}`);
-      racketbuf += data;
-      try {
-        const resultJSON = JSON.parse(racketbuf);
-        console.log(JSON.stringify(resultJSON));
-        resultJSON["program-points"] = resultJSON["program-points"].filter(x => x.syntax.source !== false)
-        resultJSON["rejected-states"] = resultJSON["rejected-states"].map(x => {x.path = x.path.filter(y => y.source !== false); return x;});
-        resultJSON["rejected-states"] = resultJSON["rejected-states"].map(x => {x.stack = x.stack.filter(y => y.source !== false); return x;});
-        resultJSON["solutions"] = resultJSON["solutions"].map(x => {x.path = x.path.filter(y => y.source !== false); return x;});
-        resultJSON["solutions"] = resultJSON["solutions"].map(x => {x.stack = x.stack.filter(y => y.source !== false); return x;});
-        console.log(JSON.stringify(resultJSON));
-        res.json(resultJSON);
-        res.end();
-      } catch (e) {
-        // ignore cause incomplete json, keep buffering
-      }
-    });
+  racket.stdout.on('data', (data) => {
+    // console.log(`$stdout: ${data}`);
+    racketBuf += data;
+    try {
+      const resultJSON = JSON.parse(racketBuf);
+      // console.log(JSON.stringify(resultJSON));
+      resultJSON["program-points"] = resultJSON["program-points"].filter(x => x.syntax.source !== false)
+      resultJSON["rejected-states"] = resultJSON["rejected-states"].map(x => {x.path = x.path.filter(y => y.source !== false); return x;});
+      resultJSON["rejected-states"] = resultJSON["rejected-states"].map(x => {x.stack = x.stack.filter(y => y.source !== false); return x;});
+      resultJSON["solutions"] = resultJSON["solutions"].map(x => {x.path = x.path.filter(y => y.source !== false); return x;});
+      resultJSON["solutions"] = resultJSON["solutions"].map(x => {x.stack = x.stack.filter(y => y.source !== false); return x;});
+      // console.log(JSON.stringify(resultJSON));
+      res.json(resultJSON);
+      res.end();
+    } catch (e) {
+      // ignore because incomplete json, keep buffering
+    }
+  });
 
   racket.stderr.on('data', (data) => {
     console.error(`stderr: ${data.toString()}`);
-    res.status(400).send(data.toString());
+    res.sendStatus(400);
+    res.status(400).write(data.toString());
+    res.end();
+
+    racket.stdout.removeAllListeners();
+    racket.stderr.removeAllListeners();
     racket.kill();
     racket = null;
   });
--- backend/repl.rkt
+++ backend/repl.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
-(define-namespace-anchor godWhyIs)
-(define racketSoHard (namespace-anchor->namespace godWhyIs))
+(define-namespace-anchor replAnchor)
+(define replNamespace (namespace-anchor->namespace replAnchor))
 
 (require json)
 (require (file "C:\\Users\\daksh\\WebstormProjects\\mk_debugger\\debugger\\mk-fo.rkt"))
-(include (file "C:\\Users\\daksh\\WebstormProjects\\relational-float\\mk-float.rkt"))
+(include (file "C:\\Users\\daksh\\Downloads\\proofo.rkt"))
 ; potentially other includes possible here for multi file debugging
 
 (define (input-loop)
@@ -18,14 +18,15 @@
                (samples (hash-ref json-command 'samples))
                (query (hash-ref json-command 'query)))
 
-              (eval (read (open-input-string (string-append "(run " solutions " " steps " " samples " #t " query")"))) racketSoHard)
+              (eval (read (open-input-string (string-append "(run " solutions " " samples " " steps " #t " query")"))) replNamespace)
               (flush-output))
          (input-loop)]
         [(string=? (hash-ref json-command 'command) "resume")
          (let ((solutions (hash-ref json-command 'solutions))
                (steps (hash-ref json-command 'steps))
                (samples (hash-ref json-command 'samples)))
-              (eval (read (open-input-string (string-append "(resume " solutions " " steps " " samples " #t)"))) racketSoHard)
+
+              (eval (read (open-input-string (string-append "(resume " solutions " " samples " " steps " #t)"))) replNamespace)
               (flush-output))
          (input-loop)]
         [(string=? (hash-ref json-command 'command) "exit") (exit 0)] ; not really necessary
