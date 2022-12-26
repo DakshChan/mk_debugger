f485da6e5c3be5df9e55c43821bdf9b3ac1e326e
Daksh <daksh3451@gmail.com>
2022-12-26 18:17:08 -0500

    fix point panel transition

--- frontend/src/PointInfoPanel.js
+++ frontend/src/PointInfoPanel.js
@@ -28,22 +28,18 @@ export default function PointInfoPanel({pointDebug, code}) {
         <CloseButton onClick={onClose}/>
       </div>
       <Collapse in={isOpen} animateOpacity>
-        { state === undefined ? <></> :
-        <>
-          <div style={{display: "flex", gap: "3ch"}}>
-           <p style={{whiteSpace: "nowrap"}}>Encounters: {state.count}</p>
-           <p style={{whiteSpace: "nowrap"}}>Failures: {state.fails}</p>
-           <p style={{whiteSpace: "nowrap"}}>Successes: {state.successes}</p>
-          </div>
-          <div style={{display: "flex", gap: "3ch"}}>
-           <p style={{whiteSpace: "nowrap"}}>Line: {state.syntax.line}</p>
-           <p style={{whiteSpace: "nowrap"}}>Column: {state.syntax.column}</p>
-          </div>
-          <div>
-             <p style={{whiteSpace: "break-spaces"}}>Syntax: {state.syntax.content}</p>
-          </div>
-        </>
-        }
+        <div style={{display: "flex", gap: "3ch"}}>
+         <p style={{whiteSpace: "nowrap"}}>Encounters: {state?.count}</p>
+         <p style={{whiteSpace: "nowrap"}}>Failures: {state?.fails}</p>
+         <p style={{whiteSpace: "nowrap"}}>Successes: {state?.successes}</p>
+        </div>
+        <div style={{display: "flex", gap: "3ch"}}>
+         <p style={{whiteSpace: "nowrap"}}>Line: {state?.syntax.line}</p>
+         <p style={{whiteSpace: "nowrap"}}>Column: {state?.syntax.column}</p>
+        </div>
+        <div>
+           <p style={{whiteSpace: "break-spaces"}}>Syntax: {state?.syntax.content}</p>
+        </div>
       </Collapse>
     </div>
   );
