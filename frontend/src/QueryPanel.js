c66e729fa43c71aed7794c407340cf45b5306f7c
Daksh <daksh3451@gmail.com>
2023-01-02 16:32:18 -0500

    added accordion for queries

--- frontend/src/QueryPanel.js
+++ frontend/src/QueryPanel.js
@@ -1,7 +1,10 @@
 import QueryForm from "./QueryForm";
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
-import { Button } from "@chakra-ui/react";
+import {
+  Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel,
+  Box, Button
+} from "@chakra-ui/react";
 import { socket } from "./socket";
 
 export default function QueryPanel({setQueries}) {
@@ -16,15 +19,27 @@ export default function QueryPanel({setQueries}) {
   }
 
   return (
-    <div className={"query-panel"} style={{display: "flex", flexDirection: "column", gap: "0.2em"}}>
-      {
-        forms.map((id, index) => {
-          return <QueryForm key={id} id={id} index={index} removeForm={removeForm} setQueries={setQueries}/>;
-        })
-      }
-      <div>
-        <Button size={'sm'} onClick={() => {setForms([...forms, uuidv4()])}}>Add Query</Button>
-      </div>
+    <div>
+      <Accordion defaultIndex={[0]} allowToggle>
+        <AccordionItem>
+          <AccordionButton>
+            <Box as="span" flex='1' textAlign='left'>
+              Queries
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel padding={'0'} style={{display: "flex", flexDirection: "column", gap: "0.2em"}}>
+            {
+              forms.map((id, index) => {
+                return <QueryForm key={id} id={id} index={index} removeForm={removeForm} setQueries={setQueries}/>;
+              })
+            }
+            <div>
+              <Button size={'sm'} onClick={() => {setForms([...forms, uuidv4()])}}>Add Query</Button>
+            </div>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
